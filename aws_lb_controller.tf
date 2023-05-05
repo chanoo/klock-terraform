f0199ab90af3308e38c6219d9d20d23c6b06b29d
“chanoo” <x64core@gmail.com>
2023-05-05 21:35:49 +0900

    AWS Load Balancer Controller Helm 차트 버전 업데이트 1.5.2

--- aws_lb_controller.tf
+++ aws_lb_controller.tf
@@ -60,7 +60,7 @@ resource "helm_release" "aws_load_balancer_controller" {
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
-  version    = "1.4.8"
+  version    = "1.5.2"
   namespace  = "kube-system"
   timeout    = 600
 
