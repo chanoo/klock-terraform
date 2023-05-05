c360966cbe120f665aff0a4549a9f36a2a02e0a5
“chanoo” <x64core@gmail.com>
2023-05-05 20:59:03 +0900

    NAT 1개로 수정

--- vpc.tf
+++ vpc.tf
@@ -35,46 +35,43 @@ resource "aws_subnet" "this" {
 }
 
 resource "aws_eip" "nat" {
-  count = 2
-
   vpc = true
 
   tags = {
-    Name = "eip-nat-${count.index + 1}"
+    Name = "eip-nat-1"
   }
 }
 
 resource "aws_nat_gateway" "this" {
-  count = 2
-
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.this["public-${["a", "b"][count.index]}"].id
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.this["public-a"].id
 
   tags = {
-    Name = "nat-${["a", "b"][count.index]}"
+    Name = "nat-a"
   }
 }
 
 resource "aws_route_table" "private" {
-  count = 2
-
   vpc_id = aws_vpc.this.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this[count.index].id
+    nat_gateway_id = aws_nat_gateway.this.id
   }
 
   tags = {
-    Name = "private-${["a", "b"][count.index]}"
+    Name = "private"
   }
 }
 
-resource "aws_route_table_association" "private" {
-  count = 2
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.this["private-a"].id
+  route_table_id = aws_route_table.private.id
+}
 
-  subnet_id      = aws_subnet.this["private-${["a", "b"][count.index]}"].id
-  route_table_id = aws_route_table.private[count.index].id
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.this["private-b"].id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_internet_gateway" "this" {
@@ -122,7 +119,6 @@ resource "aws_iam_policy" "amazon_vpc_cni_policy" {
       }
     ]
   })
-
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_vpc_cni_attachment" {
